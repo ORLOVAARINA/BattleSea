6f6fcd4703e2a702db8e237f6ed52910625d59a6
ORLOVAARINA <Pleasvood@gmail.com>
2024-12-16 05:59:43 +0300

    commit

--- config_data/config.py
+++ config_data/config.py
@@ -1,28 +1,18 @@
-import logging
-import os
-from dotenv import load_dotenv
+import configparser
 from aiogram.types import User
+import logging
 
 # Настройка логирования
-logger = logging.getLogger(__name__)  # Используем __name__ для более информативного имени логера
+logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-# Загрузка переменных окружения из файла .env
-load_dotenv()
-
-# Получение значений из переменных окружения. Обработка отсутствующих значений.
-token_id = os.getenv('token')
-admin_ids_str = os.getenv('admin_id')
-
-if not token_id:
-    logger.critical("token не найден в .env файле!")
-    exit(1)
-
-try:
-    admin_id = [int(x) for x in admin_ids_str.split(',')] if admin_ids_str else [] #Обработка случая, если ADMIN_IDS пустой.
-except (AttributeError, ValueError):
-    logger.error("Неверный формат ADMIN_IDS в .env файле!")
-    admin_id = []
+# Создание объекта для работы с конфигурационными файлами
+config = configparser.ConfigParser()
+config.read('.env', encoding='utf-8')
+data = config['settings']
+token = data['token']
+admin_id = list(map(int, data['admin_id'].split(',')))
+weather_api_key = data.get('weather_api_key')
 
 # Создаем класс для хранения данных бота
 class BotData:
@@ -30,11 +20,13 @@ class BotData:
         self.bot_username = None
         self.bot_name = None
 
-bot_data = BotData()  # Создаем экземпляр класса BotData
+bot_data = BotData() # Создаем экземпляр класса BotData для хранения данных бота
 
 # Асинхронная функция для обновления данных бота
 async def update_data(user: User):
-    bot_data.bot_username = user.username  # Обновляем данные через экземпляр класса
-    bot_data.bot_name = user.first_name
-    logger.info(f"Данные бота обновлены: @{bot_data.bot_username}, {bot_data.bot_name}") # Логируем обновление данных.
+    global bot_username
+    global bot_name
 
+    bot_username = user.username
+    bot_name = user.first_name
+    print(f"@{bot_username}", bot_name)
